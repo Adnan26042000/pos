6419820fb5416bdc8cedafcb270fc7d7a527a11e
code now <ma382793@gmail.com>
2023-08-21 09:57:23 -0700

    here

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,39 +1,39 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title') {{ config('app.name', 'POS') }}</title>
+    <title>@yield('title') {{ config('app.name', 'POS') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        @livewireStyles
-        <style>
-            [x-cloak] {
-                display: none !important;
-            }
-        </style>
-        @livewireScripts
-        <!-- Scripts -->
-        <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased h-full bg-gray-100">
-    <div x-data="{sidebar: false}" x-cloak>
-        {{--   I'm using sidebar variable in header and sidebar both files--}}
-        @include('includes.sidebar')
-        <div class="lg:pl-60">
-                    @include('includes.header')
-            <main class="py-10">
-                <div class="px-4 sm:px-6 lg:px-8">
-                    @yield('content')
-                </div>
-            </main>
-        </div>
+    @livewireStyles
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
+    @livewireScripts
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+<body class="font-sans antialiased h-full bg-gray-100">
+<div x-data="{sidebar: false}" x-cloak>
+    {{--   I'm using sidebar variable in header and sidebar both files--}}
+    @include('includes.sidebar')
+    <div class="lg:pl-72">
+        @include('includes.header')
+        <main class="py-10">
+            <div class="px-4 sm:px-6 lg:px-8">
+                @yield('content')
+            </div>
+        </main>
     </div>
-    </body>
+</div>
+</body>
 </html>
